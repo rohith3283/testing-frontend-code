cdefc47fee64aee1dad618b902fc6f9c86f8cbe8
rohith3283 <109816311+rohith3283@users.noreply.github.com>
2023-01-19 22:31:35 +0530

    comment added

---
+++ app.js
@@ -0,0 +1,207 @@
+
+// const red = document.querySelector("#red");
+// const blue = document.querySelector("#blue");
+// const green = document.querySelector("#green");
+// const inputText = document.querySelector("#input");
+// const displayText = document.querySelector("#displayinput");
+
+// // console.log("inputValue", inputValue)
+
+// red.addEventListener("mouseover", function() {
+//     inputText.style.color = "red";
+
+// });
+
+// blue.addEventListener("mouseover", function() {
+
+//     inputText.style.color = "blue";
+// });
+
+// green.addEventListener("mouseover", function() {
+
+
+
+//     inputText.style.color = "green";
+// });
+
+
+// red.addEventListener("click", function() {
+//     let inputText = document.querySelector("#input");
+//     // console.log("inputText", inputText);
+//     let inputValue = inputText.value;
+//     displayText.innerText = inputValue;
+//     displayText.style.color = "red";
+// });
+
+// blue.addEventListener("click", function() {
+
+//     let inputText = document.querySelector("#input");
+//     // console.log("inputText", inputText);
+//     let inputValue = inputText.value;
+//     displayText.innerText = inputValue;
+//     displayText.style.color = "blue";
+    
+// });
+
+// green.addEventListener("click", function() {
+
+//     let inputText = document.querySelector("#input");
+//     // console.log("inputText", inputText);
+//     let inputValue = inputText.value;
+//     displayText.innerText = inputValue;
+//     displayText.style.color = "green";
+    
+// });
+
+// 1st qst computation
+// let addBtn = document.querySelector("#add");
+// let subBtn = document.querySelector("#sub");
+// let mulBtn = document.querySelector("#mul");
+// let divBtn = document.querySelector("#div");
+
+// let result = document.querySelector("#output");
+
+//  addBtn.addEventListener("click", function() {
+
+//     let a = +document.querySelector("#num1").value;
+//     let b = +document.querySelector("#num2").value;
+    
+// // console.log("a", a);
+// // console.log("b", b);
+//   let c = a + b;
+//   result.innerText = c
+
+//  });
+
+//  subBtn.addEventListener("click", function() {
+
+//     let a = +document.querySelector("#num1").value;
+//     let b = +document.querySelector("#num2").value;
+    
+// // console.log("a", a);
+// // console.log("b", b);
+//   let c = a - b;
+//   result.innerText = c
+
+//  });
+
+//  mulBtn.addEventListener("click", function() {
+
+//     let a = +document.querySelector("#num1").value;
+//     let b = +document.querySelector("#num2").value;
+    
+// // console.log("a", a);
+// // console.log("b", b);
+//   let c = a * b;
+//   result.innerText = c
+
+//  });
+
+//  divBtn.addEventListener("click", function() {
+
+//     let a = +document.querySelector("#num1").value;
+//     let b = +document.querySelector("#num2").value;
+    
+// // console.log("a", a);
+// // console.log("b", b);
+//   let c = a / b;
+//   result.innerText = c
+
+//  });
+
+// 2nd que
+
+
+// let addbtn = document.querySelector("#increse");
+// let subbtn = document.querySelector("#decrese");
+
+// addbtn.addEventListener("click", function() {
+//     let inputText = document.querySelector("#text");
+    // console.log("inputText", inputText.style.fontSize, inputText.size);
+    // inputText.style.fontSize = parseFloat(inputText.style.fontSize || 10) + 2 + 'px';
+   
+
+// })
+
+// 3rd ques
+
+// let firstBtn = document.querySelector("#h11");
+// let secBtn = document.querySelector("#h22");
+// let thirdBtn = document.querySelector("#h33");
+
+
+// firstBtn.addEventListener("click", function() {
+//     let inputText = document.querySelector("#text");
+//     inputText.style.fontSize = "32px";
+// } );
+
+// secBtn.addEventListener("click", function() {
+//     let inputText = document.querySelector("#text");
+//     inputText.style.fontSize = "24px";
+// } );
+
+// thirdBtn.addEventListener("click", function() {
+//     let inputText = document.querySelector("#text");
+//     inputText.style.fontSize = "18.72px";
+// } );
+
+
+// 6th ques
+
+// let loadingBtn = document.querySelector("#loadBtn");
+// let textBtn = document.querySelector("#para");
+
+
+// loadingBtn.addEventListener("click", function() {
+//     textBtn.style.display = "none";
+// })
+
+
+// 11th
+
+
+// let loadBtn = document.querySelector("#load");
+
+// loadBtn.addEventListener("click", function() {
+//     let inputText = document.querySelector("#text").value;
+//     let inputColor = document.querySelector("#text");
+//     if (inputText.length < 10) {
+//         window.alert("error");
+//         inputColor.style.backgroundColor = "red";
+//         loadBtn.setAttribute("disabled", "");
+        
+//     }
+//     else 
+//   {
+//     window.alert("success");
+//     inputColor.style.backgroundColor = "green";
+//     loadBtn.removeAttribute('disabled', "")
+//   }
+// })
+// document.querySelector('#text').addEventListener("change", function(e) {
+//     let inputText = document.querySelector("#text").value;
+//     let inputColor = document.querySelector("#text");
+//     console.log("e.target.value", e.target.value)
+//     if (inputText.length < 10) {
+//         window.alert("error");
+//         inputColor.style.backgroundColor = "red";
+//         loadBtn.setAttribute("disabled", "");
+        
+//     }
+//     else 
+//   {
+//     window.alert("success");
+//     inputColor.style.backgroundColor = "green";
+//     loadBtn.removeAttribute('disabled', '')
+//   }
+// })
+
+// let oneBtn = document.querySelector("#p1");
+// let twoBtn = document.querySelector("#p2");
+// let h1 = document.querySelector("h1");
+
+// oneBtn.addEventListener("click", function() {
+//     h1.setAttribute("class", "heading1");
+   
+// } )
+
---
+++ test.js
@@ -0,0 +1,23 @@
+// let a  = {
+//     name: "ram",
+//     age: 25,
+//     yuga: "sdbfh"
+// }
+
+// let b = {
+//     name: "krishna",
+//     age: 31,
+//     yuga: "vsdjvnsjdv"
+// }
+
+// function fOne(obj1, obj2){
+//     if(obj1["age"] > obj2["age"]){
+//         return obj1;
+//     }else if(obj1["age"] < obj2["age"]){
+//         return obj2;
+//     }else{
+//         return "equal";
+//     }
+// }
+
+// console.log(fOne(a, b));
